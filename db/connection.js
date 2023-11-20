4a49ff2fae3bd36480b79bffe659b5a140de9f15
dannygorgon <thirdoptionisthename@googlemail.com>
2023-11-20 11:21:07 +0000

    add README.md. edit package.json, add.env files

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,6 @@
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
+const PGDATABASE = process.env.PGDATABASE
 
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
@@ -8,5 +9,7 @@ require('dotenv').config({
 if (!process.env.PGDATABASE) {
   throw new Error('PGDATABASE not set');
 }
-
+console.log(`the node environment is...${ENV}`);
+console.log(`the path is ${__dirname}/../.env.${ENV}`);
+console.log(`the database is ${PGDATABASE}`);
 module.exports = new Pool();
---
+++ listen.js
@@ -0,0 +1,5 @@
+const app = require("./app")
+const port = 9090
+app.listen(port, () => {
+    console.log(`You are now connected on port ${port}`)
+}) 
