e0e237fdfb143e7b91d269156c1ec360c8171ae5
dannygorgon <thirdoptionisthename@googlemail.com>
2023-11-28 17:56:31 +0000

    add test case for question 12

--- models/models.js
+++ models/models.js
@@ -18,15 +18,19 @@ const getArticlesByID = (id) => {
   }
   return db
     .query(
-      `SELECT articles.author, articles.title, articles.article_id, articles.body, articles.topic, articles.created_at, articles.votes, articles.article_img_url FROM articles
-  WHERE article_id = $1;`,
-      [id]
+      `SELECT articles.*, COUNT(comments.comment_id)::INT AS comment_count FROM articles
+      LEFT JOIN comments ON articles.article_id = comments.article_id
+      WHERE articles.article_id = $1
+      GROUP BY articles.article_id;`, // only need to group by unique value
+          [id]
     )
     .then((data) => {
       if (!data.rows.length || data.rows.length === 0) {
         return Promise.reject({ status: 404, msg: "Not found" });
       }
-      return data.rows[0];
+      const article = data.rows[0];
+      console.log(article);
+      return article;
     });
 };
 
